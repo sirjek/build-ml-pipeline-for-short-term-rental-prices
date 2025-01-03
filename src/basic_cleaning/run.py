fcafd7dc39af48d943244aa9f8709c62d1f0668c
eugene-akiro <eugene.kadiri@akirolabs.com>
2025-01-03 22:56:00 +0100

    Model Pipeline

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -37,6 +37,9 @@ def go(args):
 
     logger.info("Creating artifact")
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv("clean_sample.csv", index=False)
 
     artifact = wandb.Artifact(
